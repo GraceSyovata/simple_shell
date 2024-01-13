97c8051d62c7eccad75af8df9ee7c384dcfb7037
CPA Grace Syovata <123548408+GraceSyovata@users.noreply.github.com>
2024-01-13 03:05:43 +0300

    Add files via upload
    Alx simple shell

---
+++ cmp.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/**
+ * _strncmp - to compare n bytes in str1 and str2
+ * @str1: 1st string for comparison
+ * @str2: second string for comparison
+ * @n: no of bytes for checking
+ * Return: > 0 if str2 is less than str1, < 0 if str1 < str2,
+ * 0 is str1 is equivalent to str2
+ */
+
+int _strncmp(char *str1, char *str2, int n)
+{
+	if (!n)
+		return (0);
+	if (*str1 == *str2)
+		return (*str1 ? _strncmp(str1 + 1, str2 + 1, n - 1) : 0);
+	if (*str1)
+		return (1);
+	if (*str2)
+		return (-1);
+	return (*str1 - *str2);
+}
